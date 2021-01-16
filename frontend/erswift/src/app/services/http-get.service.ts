cf38b328d5efc207ac75cdaa366fb85acaaab14e
Brian Han <brihan.tech@gmail.com>
2021-01-16 08:44:45 -0800

    Http Get Service

---
+++ frontend/erswift/src/app/services/http-get.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { IHospitalArea } from '../interfaces/ihospital-area';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpGetService {
+
+  private erswiftAPIUrl = "http://127.0.0.1:8000/api/";
+
+  constructor(private http: HttpClient) { }
+
+  getQueue(id:number): Observable<IHospitalArea> {
+    return this.http.get<IHospitalArea>(this.erswiftAPIUrl + "hospital-areas/hospital-priority-areas/" + id + "/").pipe(
+      catchError(this.handleError)
+    )
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    let errorMessage = "";
+    if (err.error instanceof ErrorEvent) {
+        errorMessage = "An error has occured: " + err.error.message;
+    } else {
+        errorMessage = "Server returned code: " + err.status + " error message is " + err.message;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
+}
