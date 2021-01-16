3ce7a0bb624bd351794b6f455c493073fd7591b1
Brian Han <brihan.tech@gmail.com>
2021-01-16 08:44:24 -0800

    Queue Component Routes

--- frontend/erswift/src/app/app-routing.module.ts
+++ frontend/erswift/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmergencyCheckInComponent } from './emergency-check-in/emergency-check-in.component';
 import { HomeComponent } from './home/home.component';
+import { QueuePositionComponent } from './queue-position/queue-position.component';
 
 const routes: Routes = [
-  { path: "", component: HomeComponent},
-  { path: "emergency-check-in", component: EmergencyCheckInComponent}
+  { path: "", component: HomeComponent },
+  { path: "emergency-check-in", component: EmergencyCheckInComponent },
+  { path: "queue-position/:id", component: QueuePositionComponent }
 ];
 
 @NgModule({
