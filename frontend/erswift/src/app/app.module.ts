7c88be5b856eaa6be4b4d069d1dc70969c24475d
Brian Han <brihan.tech@gmail.com>
2021-01-16 01:17:19 -0800

    Emergency Check In

--- frontend/erswift/src/app/app.module.ts
+++ frontend/erswift/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { EmergencyCheckInComponent } from './emergency-check-in/emergency-check-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    EmergencyCheckInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
