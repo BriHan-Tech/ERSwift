75d8723b8f6c0d281df510f6700f233829cf621b
Brian Han <brihan.tech@gmail.com>
2021-01-15 22:54:30 -0800

    Changed DB

--- backend/erswift/patient/models.py
+++ backend/erswift/patient/models.py
@@ -4,7 +4,7 @@ patient/models.py
 """
 
 from django.db import models
-from hospital.models import HospitalArea
+from hospital.models import PriorityHospitalArea, HospitalArea
 
 
 class PriorityPatient(models.Model):
@@ -22,11 +22,13 @@ class PriorityPatient(models.Model):
     first_name = models.CharField(max_length=26)
     last_name = models.CharField(max_length=50)
     date_of_birth = models.CharField(max_length=30)
+    
+    current_time = models.DateTimeField(auto_now=True)
 
     emer_reasoning = models.CharField(choices=PRIORITY_CHOICES, max_length=15)
     extra_information = models.TextField()
 
-    hospital_area = models.ForeignKey(HospitalArea, related_name="priority_patient", on_delete=models.CASCADE)
+    hospital_area = models.ForeignKey(PriorityHospitalArea, related_name="priority_patients", on_delete=models.CASCADE)
 
     def __str__(self):
         return "{0} {1}".format(self.first_name, self.last_name)
@@ -56,12 +58,14 @@ class Patient(models.Model):
     first_name = models.CharField(max_length=26)
     last_name = models.CharField(max_length=50)
     date_of_birth = models.CharField(max_length=30)
+
+    current_time = models.DateTimeField(auto_now=True)
     
     emer_reasoning = models.CharField(choices=INJURY_SYMPTOMS, max_length=11)
     cut_location = models.CharField(choices=CUT_LOCATION, max_length=5, null=True, blank=True)
     extra_information = models.TextField()
 
-    hospital_area = models.ForeignKey(HospitalArea, related_name="patient", on_delete=models.CASCADE)
+    hospital_area = models.ForeignKey(HospitalArea, related_name="patients", on_delete=models.CASCADE)
 
     def __str__(self):
         return "{0} {1}".format(self.first_name, self.last_name)
