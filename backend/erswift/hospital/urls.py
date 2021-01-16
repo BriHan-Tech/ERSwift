94a33076b757c72ca8af018602d2080e2f640203
Brian Han <brihan.tech@gmail.com>
2021-01-15 22:57:13 -0800

    urls

--- backend/erswift/hospital/urls.py
+++ backend/erswift/hospital/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .api import HospitalAreaViewSet
+from .api import HospitalAreaViewSet, PriorityHospitalArea
 
 ROUTER = DefaultRouter()
+ROUTER.register("hospital-priority-areas", PriorityHospitalArea)
 ROUTER.register("hospital-areas", HospitalAreaViewSet)
 
 urlpatterns = ROUTER.urls
