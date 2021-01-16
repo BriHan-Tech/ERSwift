94378699f846591336148b0a6d24dfd832cd16fb
Brian Han <brihan.tech@gmail.com>
2021-01-16 09:52:04 -0800

    Q Position Logic

--- frontend/erswift/src/app/queue-position/queue-position.component.ts
+++ frontend/erswift/src/app/queue-position/queue-position.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { interval, Observable } from 'rxjs';
 import { HttpGetService } from '../services/http-get.service';
 
 @Component({
@@ -9,13 +10,39 @@ import { HttpGetService } from '../services/http-get.service';
 })
 export class QueuePositionComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, private httpGetService: HttpGetService) { }
+  constructor(private httpGetService: HttpGetService) { 
+    interval(3000).subscribe(x => {
+      this.getQueue(this.hospital_area_id);
+    })
+  }
+
+  hospital_area_id:number;
+  user_id: string;
+
+  hospitalArea: any;
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(
-      
-    )
+    this.hospital_area_id = +localStorage.getItem("hospital_area");
+    this.user_id = localStorage.getItem("user_id");
+    this.getQueue(this.hospital_area_id);
     
+  }
+
+  getQueue(id:number):void {
+    this.httpGetService.getQueue(id).subscribe(
+      hospital_area => {
+        this.hospitalArea = hospital_area;
+      }
+    )
+  }
+
+  queueNumber():number {
+    const queueList = this.hospitalArea.priority_patients;
+    for (let i=0; i < queueList.length; i ++) {
+      if (this.user_id == queueList[i].id) {
+        return i+1
+      }
+    }
   }
 
 }
