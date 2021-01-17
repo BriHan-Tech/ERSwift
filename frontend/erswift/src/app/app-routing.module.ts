4fa67b2e737d0aa10db12cd99c1db9088f2bea28
Brian Han <brihan.tech@gmail.com>
2021-01-17 01:01:49 -0800

    Routing to Error Pages

--- frontend/erswift/src/app/app-routing.module.ts
+++ frontend/erswift/src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { BackButtonShellComponent } from './back-button-shell/back-button-shell.component';
 import { CheckInComponent } from './check-in/check-in.component';
 import { EmergencyCheckInComponent } from './emergency-check-in/emergency-check-in.component';
+import { BadRequestComponent } from './error-pages/bad-request/bad-request.component';
+import { ForbiddenComponent } from './error-pages/forbidden/forbidden.component';
 import { NotFoundComponent } from './error-pages/not-found/not-found.component';
 import { ServerErrorComponent } from './error-pages/server-error/server-error.component';
 import { HomeComponent } from './home/home.component';
@@ -25,6 +27,8 @@ const routes: Routes = [
   },
   { path: "404", component: NotFoundComponent},
   { path: "500", component: ServerErrorComponent },
+  { path: "400", component: BadRequestComponent },
+  { path: "403", component: ForbiddenComponent },
   { path: "**", redirectTo:"404", pathMatch:"full"}
 ];
 
