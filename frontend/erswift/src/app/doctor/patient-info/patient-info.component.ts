df518513d551e26244b5155303500631de5e177d
Brian Han <brihan.tech@gmail.com>
2021-01-16 17:30:15 -0800

    Patient Info Component

--- frontend/erswift/src/app/doctor/patient-info/patient-info.component.ts
+++ frontend/erswift/src/app/doctor/patient-info/patient-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpGetService } from 'src/app/services/http-get.service';
 
 @Component({
   selector: 'app-patient-info',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientInfoComponent implements OnInit {
 
-  constructor() { }
+  triage: string;
+  patient: any;
+
+  constructor(private route: ActivatedRoute, private httpGetService: HttpGetService) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(
+      params => {
+        const id = +params.get('id');
+        this.triage = params.get('triage');
+        this.getPatient(id);
+      }
+    )
+  }
+
+  getPatient(id:number):void {
+    if (this.triage == "priority") {
+      this.httpGetService.getPatientFromPriority(id).subscribe(
+        patient => {
+          this.patient = patient;
+          console.log(this.patient)
+        }
+      )
+    } else if (this.triage == "urgent") {
+      this.httpGetService.getPatientFromNormal(id).subscribe(
+        patient => {
+          this.patient = patient;
+        }
+      )
+    }
   }
 
 }
