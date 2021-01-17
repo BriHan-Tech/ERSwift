5ca4aba1988caca86df923a728a820669ee79728
Brian Han <brihan.tech@gmail.com>
2021-01-16 23:13:06 -0800

    Back Button for Doctor Components

---
+++ frontend/erswift/src/app/doctor/back-button-shell/back-button-shell.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-back-button-shell',
+  templateUrl: './back-button-shell.component.html',
+  styleUrls: ['./back-button-shell.component.scss']
+})
+export class BackButtonShellComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
