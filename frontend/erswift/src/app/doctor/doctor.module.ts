408f588fd98c9332490c26cfa21f6db9bc59a125
Brian Han <brihan.tech@gmail.com>
2021-01-16 23:13:52 -0800

    RouteGuards

--- frontend/erswift/src/app/doctor/doctor.module.ts
+++ frontend/erswift/src/app/doctor/doctor.module.ts
@@ -5,17 +5,28 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PatientInfoComponent } from './patient-info/patient-info.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from '../services/auth.service';
+import { BackButtonShellComponent } from './back-button-shell/back-button-shell.component';
 
 
 @NgModule({
-  declarations: [DashboardComponent, PatientInfoComponent],
+  declarations: [DashboardComponent, PatientInfoComponent, LoginComponent, BackButtonShellComponent],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     RouterModule.forChild([
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'patient-info/:id', component: PatientInfoComponent }
+      {
+        path: '',
+        component: BackButtonShellComponent,
+        children: [
+          { path: 'login', component: LoginComponent },
+          { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+          { path: 'patient-info/:id', component: PatientInfoComponent, canActivate: [AuthGuard] },
+        ]
+      }
+
     ])
   ],
 })
