2a586d752b5e55e2dd4f864654c01a4842088a07
Brian Han <brihan.tech@gmail.com>
2021-01-16 23:55:01 -0800

    verify Users

--- frontend/erswift/src/app/home/home.component.ts
+++ frontend/erswift/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { HttpGetService } from '../services/http-get.service';
 
 @Component({
   selector: 'app-home',
@@ -9,13 +10,29 @@ import { AuthService } from '../services/auth.service';
 export class HomeComponent implements OnInit {
 
   user_id: number;
+  triage: number;
   username: string;
 
-  constructor(private authService: AuthService ) { }
+  constructor(private authService: AuthService, private httpGetService: HttpGetService ) { }
 
   ngOnInit(): void {
     this.user_id = +localStorage.getItem("user_id");
+    this.triage = +localStorage.getItem("triage");
+
     this.username = localStorage.getItem("username");
+
+    this.checkUser();
+  }
+
+  checkUser():void {
+    // If Patient is Dismissed, we Remove the user_id
+    if (this.user_id) {
+      if (this.triage == 1) {
+        this.httpGetService.verifyPriorityPatient(this.user_id).subscribe();
+      } else {
+        this.httpGetService.verifyPatientList(this.user_id).subscribe();
+      }
+    }
   }
 
   logout(): void {
